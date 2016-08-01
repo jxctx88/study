efa51a597b61d8fe4eb374b8e24401180c9a8b96
tongxiong.cheng <2411867257@qq.com>
2016-08-01 10:21:51 +0800

    增加Curator对zookeeper的场景使用

---
+++ java-base/src/main/java/cn/memedai/base/jvm/Memory/AccessDirectBuffer.java
@@ -0,0 +1,53 @@
+package cn.memedai.base.jvm.memory;
+
+import java.nio.ByteBuffer;
+
+/**
+ * Created by admin on 2016/7/28.
+ */
+public class AccessDirectBuffer {
+
+    public void directAccess(){
+        long starttime = System.currentTimeMillis();
+        ByteBuffer b = ByteBuffer.allocateDirect(500);
+        for (int i = 0; i < 100000; i++) {
+            for (int j = 0; j < 99; j++) {
+                b.putInt(j);
+            }
+            b.flip();
+            for (int j = 0; j < 99; j++) {
+                b.getInt();
+            }
+            b.clear();
+        }
+        long endtime = System.currentTimeMillis();
+        System.out.println("testDirectWirite:"+(endtime-starttime));
+    }
+
+
+    public void bufferAccess(){
+        long starttime = System.currentTimeMillis();
+        ByteBuffer b = ByteBuffer.allocate(500);
+        for (int i = 0; i < 100000; i++) {
+            for (int j = 0; j < 99; j++)
+                b.putInt(j);
+
+            b.flip();
+            for (int j = 0; j < 99; j++)
+                b.getInt();
+
+            b.clear();
+        }
+        long endtime = System.currentTimeMillis();
+        System.out.println("testDirectWirite:"+(endtime-starttime));
+    }
+
+    public static void main(String[] args) {
+        AccessDirectBuffer alloc = new AccessDirectBuffer();
+        alloc.bufferAccess();
+        alloc.directAccess();
+
+        alloc.bufferAccess();
+        alloc.directAccess();
+    }
+}
